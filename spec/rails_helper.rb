a28bf533155237b1cf879a432a9732d4db04fdc0
Gabriel Belgamo <ggbelgamo@outlook.com>
2021-03-30 23:00:50 -0300

    refactor: move shoulda config to a separate file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,9 +22,3 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
