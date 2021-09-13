d90a7ad8da49b824c520ac19b134ccfd3e466677
Gabriel Belgamo <ggbelgamo@outlook.com>
2021-09-13 10:53:06 -0300

    refactor: remove unnecessary migrations

--- db/migrate/20210910141741_add_cascade_delete_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddCascadeDeleteToUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :company_users, name: "fk_rails_946619ff40"
-    add_foreign_key :company_users, :users, on_delete: :cascade
-  end
-end
