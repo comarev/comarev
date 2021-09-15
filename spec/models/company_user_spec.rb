62b11a41c1b9b8516ca5094f3eccd1821d868737
Mauricio Lima <mauricio.lima@codeminer42.com>
2021-09-15 20:04:28 -0300

    feat: add role enum for CompanyUser

--- spec/models/company_user_spec.rb
+++ spec/models/company_user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe CompanyUser, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:company) }
   end
+
+  it {
+    is_expected.to define_enum_for(:role)
+      .with_values({ regular: 0, manager: 1 })
+      .backed_by_column_of_type(:integer)
+  }
 end
