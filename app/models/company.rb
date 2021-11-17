f216f7ac87572f69139c96e2c3d3cf261e05489f
Thiago Antonello Vargas <72185566+thiantonello@users.noreply.github.com>
2021-11-17 19:35:56 -0300

    feat: validate the presence of company's code attribute (#31)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
-  validates :name, :cnpj, :active, presence: true
+  validates :name, :cnpj, :code, :active, presence: true
   validates :cnpj, :code, uniqueness: { case_sensitive: false }
 
   has_many :company_users, dependent: :destroy
