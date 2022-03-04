415066d363d325a7256e751a8e1884212b4b000b
Rafael <perchespierri@gmail.com>
2022-03-04 07:29:36 -0300

    Insert examples in spec

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { FFaker::Name.unique.name }
     cnpj { FFaker::IdentificationBR.cnpj }
     address { FFaker::Address.street_name }
-    phone { FFaker.numerify("##############") }
+    phone { FFaker.numerify('##############') }
     active { true }
     discount { 10 }
   end
