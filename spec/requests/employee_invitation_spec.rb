539f88adfdd181ae4218c676e991eb13142e1838
antonio <aq.simei@gmail.com>
2022-06-03 14:17:04 -0300

    fix: add remaining tests for controller

--- spec/requests/employee_invitation_spec.rb
+++ spec/requests/employee_invitation_spec.rb
@@ -2,12 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'EmployeeInvitationController', type: :request do
   let(:user) { create(:user, :admin) }
+  let(:not_listed_user) { create(:user) }
   let(:company) { create(:company) }
+  let!(:company_user) { create(:company_user, user: user, company: company) }
   let(:headers) { { Authorization: sign_in(user) } }
+  
   describe 'POST /companies/:company_id/employee_invitations' do
-    subject(:invitation_controller) do
-      post company_employee_invitation_path(company), params: {email: "test_email@test.com"}, headers: headers
-    end 
-      it {is_expected.to have_http_status(:ok)}
+    context 'When user is no registered on Comarev' do
+      subject(:invitation_controller) do
+        post company_employee_invitation_path(company), params: {email: "test_email@test.com"}, headers: headers
+      end
+
+        it { is_expected.to eq(200) }
+      end
+
+    context 'When user already is listed on a Company'do
+      subject(:invitation_controller) do
+        post company_employee_invitation_path(company), params: {email: user.email}, headers: headers
+      end
+
+        it { is_expected.to eq(422) }
+    end
+    context 'When user is not listed on a Company' do
+      subject(:invitation_controller) do
+        post company_employee_invitation_path(company), params: {email: not_listed_user.email}, headers: headers
+      end
+
+        it { is_expected.to eq(200) }
+    end
   end
 end
