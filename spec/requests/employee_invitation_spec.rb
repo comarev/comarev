4af2e896adde040bd8cf477c883cd03f7c9be6ba
antonio <aq.simei@gmail.com>
2022-06-07 13:51:40 -0300

    test: add specs to response expectations

--- spec/requests/employee_invitation_spec.rb
+++ spec/requests/employee_invitation_spec.rb
@@ -13,10 +13,11 @@ RSpec.describe 'EmployeeInvitationController', type: :request do
           params: { email: 'test_email@test.com' }, headers: headers
       end
 
-      it 'returns status ok' do
+      it "returns status ok with 'email sent to new user' message" do
         invitation_controller
 
         expect(response).to have_http_status(:ok)
+        expect(json).to eq({message: "Email sent to new user"})
       end
     end
 
@@ -30,10 +31,11 @@ RSpec.describe 'EmployeeInvitationController', type: :request do
         create(:company_user, user: user, company: company)
       end
 
-      it 'returns status unprocessable entity' do
+      it "returns status unprocessable entity with 'already listed' message" do
         invitation_controller
 
         expect(response).to have_http_status(:unprocessable_entity)
+        expect(json).to eq({message: "User is already listed as #{company.name}'s employee"})
       end
     end
 
@@ -43,11 +45,26 @@ RSpec.describe 'EmployeeInvitationController', type: :request do
           headers: headers
       end
 
-      it 'returns status ok' do
+      it "returns status ok with user 'successfully listed' message" do
         invitation_controller
 
         expect(response).to have_http_status(:ok)
+        expect(json).to eq({message:"User successfully listed as #{company.name}'s employee"})
       end
     end
   end
 end
+
+
+
+  # context 'when the endpoint is called' do
+  #   subject(:invitation_controller) do
+  #     post company_employee_invitation_path(company), params: { email: user.email },
+  #       headers: headers
+  #   end
+  #   it 'calls the service object' do
+  #     invite_service_mock = instance_double('CheckEmployeeAssociation')
+  #     expect(invite_service_mock).to receive(:call).once
+  #     invitation_controller
+  #   end
+  # end
