f718be1fb4658f0b4817b835de1ebf99eeb00592
andreLumor <andre.moreira@codeminer42.com>
2022-06-21 17:25:59 -0300

    Create accept/refuse invite features

--- app/services/invite_employee_service.rb
+++ app/services/invite_employee_service.rb
@@ -10,6 +10,8 @@ class InviteEmployeeService < ApplicationService
       build_response("User is already listed as #{company.name}'s employee", :unprocessable_entity)
     elsif invite_already_sent?
       build_response("User was already invited to #{company.name}", :unprocessable_entity)
+    elsif invite_recently_refused?
+      build_response('Invite recently refused', :unprocessable_entity)
     else
       send_invitation_email
     end
@@ -20,33 +22,37 @@ class InviteEmployeeService < ApplicationService
   attr_reader :inviter, :email, :company
 
   def send_invitation_email
-    invitation = Invite.create!(inviter_id: inviter.id, invited_email: email)
-    if invitation
-      EmployeeInvitationMailer.with(token: invitation.invitation_token, 
-                                    email: email, 
-                                    company: company)
-                              .invite_employee.deliver_now &&
-      build_response('Invitation sent', :ok)
+    if (invitation = Invite.create!(inviter_id: inviter.id,
+      invited_email: email,
+      company_id: company.id))
+      EmployeeInvitationMailer.with(token: invitation.invitation_token,
+        email: email,
+        company: company)
+        .invite_employee.deliver_now &&
+        build_response('Invitation sent', :ok)
     else
       build_response('Invitation could not be sent', :unprocessable_entity)
     end
   end
 
   def listed_employee_for_company?
-    user = User.find_by(email: email)
-    if user
-      CompanyUser.where(user_id: user.id, company_id: company.id).any?
-    else
-      return false
-    end
+    return false unless (employee = User.find_by(email: email))
+
+    CompanyUser.where(user_id: employee.id, company_id: company.id).any?
   end
 
   def invite_already_sent?
-    Invite.available.where(invited_email: email, inviter: inviter.id).any?
+    Invite.available.where(invited_email: email,
+      inviter: inviter.id,
+      company_id: company.id)
+      .any?
   end
 
-  def build_response(message, status)
-    { message: message, status: status }
+  def invite_recently_refused?
+    Invite.recently_refused.where(invited_email: email,
+      inviter: inviter.id,
+      company_id: company.id)
+      .any?
   end
 
   def build_response(message, status)
--- db/migrate/20220615150547_create_invites.rb
+++ db/migrate/20220615150547_create_invites.rb
@@ -2,11 +2,12 @@ class CreateInvites < ActiveRecord::Migration[6.0]
   def change
     create_table :invites do |t|
       t.references :inviter, null: false, foreign_key: { to_table: :users }
+      t.references :company, null: false, foreign_key: true
 
       t.string :invitation_token, null: false
       t.string :invited_email, null: false
       t.datetime :replied_at
-      t.boolean :accepted
+      t.boolean :accepted, :default => nil
 
       t.timestamps
     end
