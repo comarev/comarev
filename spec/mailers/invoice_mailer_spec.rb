36948c0b9d8e1fb255c44fb2fad37dab6efcf1fb
Eric Araujo <ericpoloni@gmail.com>
2022-09-01 16:47:09 -0300

    Fix cops

--- spec/mailers/invoice_mailer_spec.rb
+++ spec/mailers/invoice_mailer_spec.rb
@@ -1,15 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe InvoiceMailer, type: :mailer do
-  describe "notify_user_email" do
+  describe 'notify_user_email' do
+    subject(:mail) { described_class.notify_user_email(user) }
 
-    subject(:mail) { InvoiceMailer.notify_user_email(user) }
     let(:user) { create(:user) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Comarev - Lembrete de expiração de fatura.")
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Comarev - Lembrete de expiração de fatura.')
       expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["test@test.com"])
+      expect(mail.from).to eq(['test@test.com'])
     end
   end
 end
--- spec/mailers/previews/invoce_mailer_preview.rb
+++ spec/mailers/previews/invoce_mailer_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invoce_mailer
 class InvoceMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/invoce_mailer/notify_user_email
   def notify_user_email
     InvoceMailer.notify_user_email
   end
-
 end
